28917d80b83a99b1589894fdd21e154e688608f8
Wattegamabc <it24610328@my.sliit.lk>
2026-03-30 00:16:25 +0530

    SCRUM-129 Treatment Process Management

--- lumberyard-backend/src/main/java/com/lumberyard_backend/dto/ProductionStatusRequest.java
+++
@@ -1,9 +0,0 @@
-package com.lumberyard_backend.dto;
-
-import com.lumberyard_backend.entity.ProductionStatus;
-import lombok.Data;
-
-@Data
-public class ProductionStatusRequest {
-    private ProductionStatus status;
-}
